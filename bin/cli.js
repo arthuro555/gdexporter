50de247fe2e64ec62ff980e62acb566f9d5732a9
arthuro555 <arthur.pacaud@hotmail.fr>
2020-06-26 20:07:10 +0200

    Refactor to authorize programmatic usage

--- bin/cli.js
+++ bin/cli.js
@@ -1,42 +1,9 @@
 const argv = require('minimist')(process.argv.slice(2));
-const projectLoader = require("../loadProject");
-const exporter = require("../GDTools/ExportProject");
+const { exporter } = require("../main");
 
-let projectPromise = projectLoader.load(argv["p"] || argv["project"] || argv["in"]);
-let outputDir = argv["o"] || argv["out"] || "./Exported";
 
+const projectDir = argv["p"] || argv["project"] || argv["in"];
+const outputDir = argv["o"] || argv["out"] || "./Exported";
 const buildType = argv["build"] || argv["b"];
 
-switch (buildType) {
-    case "electron":
-        console.log("Exporting for electron")
-        projectPromise
-            .then(project => {
-                exporter.exportPIXI(project, outputDir, {'exportForElectron': true})
-            });
-        break;
-    
-    case "cordova":
-        console.log("Exporting for cordova")
-        projectPromise
-            .then(project => {
-                exporter.exportPIXI(project, outputDir, {'exportForCordova': true})
-            });
-        break;
-
-    case "facebook":
-        console.log("Exporting for facebook instant games")
-        projectPromise
-            .then(project => {
-                exporter.exportPIXI(project, outputDir, {'exportForFacebookInstantGames': true})
-            });
-        break;
-
-    default:
-        console.log("Exporting for HTML5 (default)")
-        projectPromise
-            .then(project => {
-                exporter.exportPIXI(project, outputDir)
-            });
-}
-
+exporter(projectDir, outputDir, buildType);
---
+++ main.js
@@ -0,0 +1,40 @@
+const exporter = require("./GDTools/ExportProject");
+
+// Programmatic API
+exports.loadProject = require("./loadProject").load;
+exports.exporter = function(projectPath, outputDir, buildType) {
+    let projectPromise = exports.loadProject(projectPath);
+    switch (buildType) {
+        case "electron":
+            console.log("Exporting for electron")
+            projectPromise
+                .then(project => {
+                    exporter.exportPIXI(project, outputDir, {'exportForElectron': true})
+                });
+            break;
+        
+        case "cordova":
+            console.log("Exporting for cordova")
+            projectPromise
+                .then(project => {
+                    exporter.exportPIXI(project, outputDir, {'exportForCordova': true})
+                });
+            break;
+    
+        case "facebook":
+            console.log("Exporting for facebook instant games")
+            projectPromise
+                .then(project => {
+                    exporter.exportPIXI(project, outputDir, {'exportForFacebookInstantGames': true})
+                });
+            break;
+    
+        default:
+            console.log("Exporting for HTML5 (default)")
+            projectPromise
+                .then(project => {
+                    exporter.exportPIXI(project, outputDir)
+                });
+    }
+    return projectPromise;
+};
